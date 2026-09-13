abe36414e1b298e7f6cf5cb319aacd0ba2855407
dope-felix <destinyfelix4@gmail.com>
2026-09-13 18:06:46 -0400

    Route API authentication

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const authRoutes = require('./routes/authRoutes');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
@@ -40,6 +41,9 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes)
+
+// add authRoutes to the server by using the app.use() method.
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler

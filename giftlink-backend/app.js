d743989d558a97f8cbb8587bd7be2bbc24f57f8d
dope-felix <detinyfelix4@gmail.com>
2026-09-14 08:51:57 -0400

    Fix Linting error

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -40,7 +40,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('/api/search', searchRoutes)
+app.use('/api/search', searchRoutes);
 
 // add authRoutes to the server by using the app.use() method.
 app.use('/api/auth', authRoutes);
@@ -53,8 +53,8 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
